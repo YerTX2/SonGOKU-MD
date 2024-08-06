114cca14641b5dbb1754e652d4ad3a5eb3a31bb1
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-06 14:22:20 -0500

    ⚔️ZxYerSixZx⚔️

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -22,14 +22,14 @@ let biografia = await conn.fetchStatus(number +'@s.whatsapp.net').catch(_ => 'un
 let bio = biografia.status?.toString() || `${desc2 == '' ? lenguajeGB.smsContacto2() : desc2}`
   
 nombre = official[0][0] == String(contact[0]) ? official[0][1] : official[1][0] == String(contact[0]) ? official[1][1] : official[2][0] == String(contact[0]) ? official[2][1] : official[3][0] == String(contact[0]) ? official[3][1] : lenguajeGB.smsContacto3() 
-description = official[0][0] == String(contact[0]) ? 'FantasyBot-MD' : official[1][0] == String(contact[0]) ? 'FantasyBot-MD' : official[2][0] == String(contact[0]) ? 'FantasyBot-MD' : official[3][0] == String(contact[0]) ? 'FantasyBot-MD' : desc === '' ? 'FantasyBot-MD' : desc
+description = official[0][0] == String(contact[0]) ? 'SonGOKU-MD' : official[1][0] == String(contact[0]) ? 'SonGOKU-MD' : official[2][0] == String(contact[0]) ? 'SonGOKU-MD' : official[3][0] == String(contact[0]) ? 'SonGOKU-MD' : desc === '' ? 'SonGOKU-MD' : desc
 correo = official[0][0] == String(contact[0]) ? 'wilsmac68@gamil.com' : official[1][0] == String(contact[0]) ? 'wilsmac68@gmail.com' : official[2][0] == String(contact[0]) ? 'indefinido202@gmail.com' : mail === '' ? lenguajeGB.smsContacto6() : mail
-lugar = official[0][0] == String(contact[0]) ? '🇺🇸 Estados Unidos' : official[1][0] == String(contact[0]) ? '🇺🇸 Estados Unidos' : official[2][0] == String(contact[0]) ? '🇺🇲 Estados Unidos' : official[3][0] == String(contact[0]) ? '🇺🇸 Estados Unidos' : country === '' ? lenguajeGB.smsContacto7() : country
-enlace = official[0][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : official[1][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : official[2][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : official[3][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : md 
+lugar = official[0][0] == String(contact[0]) ? '🇵🇪 Peru' : official[1][0] == String(contact[0]) ? '🇵🇪 Peru' : official[2][0] == String(contact[0]) ? '🇵🇪 Peru' : official[3][0] == String(contact[0]) ? '🇵🇪 Peru' : country === '' ? lenguajeGB.smsContacto7() : country
+enlace = official[0][0] == String(contact[0]) ? 'https://github.com/YerTX2' : official[1][0] == String(contact[0]) ? 'https://github.com/YerTX2' : official[2][0] == String(contact[0]) ? 'https://github.com/YerTX2' : official[3][0] == String(contact[0]) ? 'https://github.com/YerTX2' : md 
    
-lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://www.youtube.com/wilmer.oficial' : null]) }  
-lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), fantasy, lenguajeGB.smsContacto8(), mail === '' ? 'wilsmac68@gmail.com' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
-await conn.sendFile(m.chat, pp, 'lp.jpg',  cat, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})
+lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://www.youtube.com/akirayt6080.oficial' : null]) }  
+lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), fantasy, lenguajeGB.smsContacto8(), mail === '' ? 'ZxYerSixZx@gmail.com' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
+await conn.sendFile(m.chat, pp, 'lp.jpg',  cat, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' ZxYerSixZx', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})
 await conn.sendContactArray(m.chat, lista, null, { quoted: fkontak })
   
 } catch (e) {
